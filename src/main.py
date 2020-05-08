233127f371d8d97df1ca71221964c45512001cb2
Deo <mcauto@naver.com>
2020-05-08 20:30:40 +0900

    feat(handler): add exception handler

--- src/main.py
+++ src/main.py
@@ -2,13 +2,21 @@
 main.py
 """
 from fastapi.applications import FastAPI
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException
 from .routes import api_v1
+from .errors import http_exception_handler, validation_exception_handler
 
 
 def create_app() -> FastAPI:
     """ app factory method """
     app = FastAPI()
     # TODO: configuration
+
+    app.add_exception_handler(
+        RequestValidationError, handler=validation_exception_handler
+    )
+    app.add_exception_handler(HTTPException, handler=http_exception_handler)
     return app
 
 
